dfbef5a64052dc2e47095889ff69184d8b4e9cae
Kapil Bavisiya <kapilbavisiya555@gmail.com>
2020-10-27 21:41:37 +0530

    second commit

---
+++ CARDS/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var card string = "Ace of Spades"
+	// for new variable... declaration
+	card := "Ace of Spades"
+	card = "Five of diamonds"
+	fmt.Println(card)
+}
