b58d5975cf7aa22109e261491d7e0d96a1fbca76
Kapil Bavisiya <kapilbavisiya555@gmail.com>
2020-10-29 20:36:33 +0530

    third

--- CARDS/main.go
+++ CARDS/main.go
@@ -1,11 +1,33 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Spades"
 	// for new variable... declaration
-	card := "Ace of Spades"
-	card = "Five of diamonds"
-	fmt.Println(card)
+	// card := "Ace of Spades"
+	// card := newCard()
+	// card = "Five of diamonds"
+	// fmt.Println(card)
+	// cards := deck{"Two of spades", newCard()}
+	// cards := newDeck()
+	// cards = append(cards, "eight of diamond")
+	// fmt.Println(cards)
+	// for i, card := range cards {
+	// 	fmt.Println(i, card)
+	// }
+	// cards.print()
+
+	// hand, remainingCards := deal(cards, 5)
+
+	// hand.print()
+	// remainingCards.print()
+
+	// greeting := "My name is Kapil"
+	// fmt.Prinln([]byte(greeting))
+	cards := newDeck()
+	// fmt.Println(cards.toString())
+	cards.saveToFile("my_cards")
 }
+
+// func newCard() string {
+// 	return "Five of Diamonds"
+// }
